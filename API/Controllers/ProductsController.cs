8254a0e60f3a994e957cbf34cb8971434f84aef0
Titas <titas.r@outlook.com>
2023-12-20 17:40:17 +0000

    first commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+
+
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+            
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await context.Products.ToListAsync();   
+
+    
+        }
+        
+        [HttpGet("id")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await context.Products.FindAsync(id);
+        }
+    }
+}
