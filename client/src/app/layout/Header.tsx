bd823c414a8c8d1a901ad68c4995bbdbf8c2988d
Titasramancauskas <titas.r@outlook.com>
2024-01-17 21:23:06 +0000

    Your commit message here

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,6 +1,7 @@
 import { Home, ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { Link, NavLink } from "react-router-dom";
+import { useStoreContext } from "../context/StoreContext";
 
 const midLinks = [
     {title: 'catalog', path: '/catalog'},
@@ -32,6 +33,8 @@ interface Props {
 }
 
 export default function Header({darkMode, handleThemeChange}: Props) {
+    const {basket} = useStoreContext();
+    const itemCount = basket?.items.reduce((sum,item) => sum + item.quantity, 0)
     return (
         <AppBar position="static" sx={{mb: 4}}>
             <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
@@ -62,7 +65,7 @@ export default function Header({darkMode, handleThemeChange}: Props) {
 
                 <Box display='flex' alignItems='center'>
                     <IconButton component={Link} to='/basket' size='large' edge='start' color='inherit' sx={{mr: 2}}>
-                        <Badge badgeContent='4' color="secondary">
+                        <Badge badgeContent={itemCount} color="secondary">
                             <ShoppingCart/>
                         </Badge>
                     </IconButton>
